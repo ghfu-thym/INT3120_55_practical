5c321d6d6bd08e67da2385c80d25f68435db6fb5
ghfu_thym <22028217@vnu.edu.vn>
2024-09-11 22:38:57 +0700

    move files

--- HW1/compareTwoNumber.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val timeSpentToday = 300
-    val timeSpentYesterday = 250
-    println(compare(timeSpentToday, timeSpentYesterday))
-}
-
-fun compare(timeSpentToday: Int, timeSpentYesterday: Int): Boolean {
-    return timeSpentToday > timeSpentYesterday
-}
