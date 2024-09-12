28d6a1437cd4cb1c1c7c18fce26e84013f7dacaf
ghfu_thym <22028217@vnu.edu.vn>
2024-09-12 14:17:09 +0700

    add HappyBirthdayCard

---
+++ Unit1/Pathway2/HappyBirthdayCard.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.HappyBirthdayTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HappyBirthdayTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday 2202!",
+                        from = "From 8217",
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    GreetingText(
+//                        message = "Happy birthday to 8217",
+//                        from = "From 2202"
+//                    )
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+            )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingText("Happy birthday 8217!", from = "From 2202")
+    }
+}
