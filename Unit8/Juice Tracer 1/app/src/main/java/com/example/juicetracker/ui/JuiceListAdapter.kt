a46be9c7c67f46951a036a2f3d9e6b22827a5831
ghfu_thym <22028217@vnu.edu.vn>
2024-10-29 22:45:41 +0700

    Unit8

---
+++ Unit8/Juice Tracer 1/app/src/main/java/com/example/juicetracker/ui/JuiceListAdapter.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.juicetracker.ui
+
+
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.juicetracker.R
+import com.example.juicetracker.data.Juice
+import com.example.juicetracker.data.JuiceColor
+
+
+
+class JuiceListAdapter(
+    private var onEdit: (Juice) -> Unit,
+    private var onDelete: (Juice) -> Unit
+) : ListAdapter<Juice, JuiceListAdapter.JuiceListViewHolder>(JuiceDiffCallback()) {
+
+    class JuiceListViewHolder(): RecyclerView.ViewHolder(){
+        private val composeView: ComposeView,
+        private val onEdit: (Juice) -> Unit,
+        private val onDelete: (Juice) -> Unit
+    } : RecyclerView.ViewHolder(composeView)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : JuiceListViewHolder {
+        return JuiceListViewHolder(
+            ComposeView(parent.context),
+        onEdit,
+        onDelete
+        )
+    }
+
+    override fun onBindViewHolder(holder: JuiceListViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+class JuiceListViewHolder(
+    private val composeView: ComposeView,
+    private val onEdit: (Juice) -> Unit,
+    private val onDelete: (Juice) -> Unit
+) : RecyclerView.ViewHolder(composeView) {
+
+
+    fun bind(juice: Juice) {
+
+    }
+}
+
+class JuiceDiffCallback : DiffUtil.ItemCallback<Juice>() {
+    override fun areItemsTheSame(oldItem: Juice, newItem: Juice): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Juice, newItem: Juice): Boolean {
+        return oldItem == newItem
+    }
+}
